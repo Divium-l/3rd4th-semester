53bae9dae3ce470eee180b648194bbfc1e69d868
Divium <79859179+Divium-l@users.noreply.github.com>
2022-02-27 23:06:22 +0300

    Тестирование будущей логики для меню
    Тестирование апкастинга

--- DataStructersCpp/DataStructersCpp.cpp
+++ DataStructersCpp/DataStructersCpp.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <concepts>
 
 /* TODO
 *   # Перенести код логики с java
@@ -14,7 +15,7 @@
 *       +  Поиск по ключу
 *       +  Вывод списка на экран
 * 
-*   # Добавить костыли для C++
+*   # Добавить функции для C++
 *       +  Деструкторы
 *       +  Кастомный поиск через лямбды
 *       +  Кастомный вывод объекта через лямбды
@@ -30,8 +31,53 @@
 
 using namespace std;
 
+class IExecutable {
+public:
+    virtual void run() = 0;// {};
+
+};
+
+class Exec1: public IExecutable {
+public:
+    virtual void run() override {
+        cout << "Exec1 " << endl;
+    }  
+
+};
+
+class Exec2 : public Exec1 {
+public:
+    virtual void run() override {
+        cout << "Exec2 " << nestedCall() << endl;
+    }
+
+private:
+    int nestedCall() {
+        return 10;
+    }
+};
+
 int main()
 {
+    //====
+    //Exec2 e2;
+    //Exec1& e1 = e2;
+    //e1.run();
+    //IExecutable& e0 = e2;
+    //e0.run();
+
+    vector<IExecutable*> vector;
+    IExecutable* e1 = new Exec1();
+    IExecutable* e2 = new Exec2();
+    vector.push_back(e1);
+    vector.push_back(e2);
+    IExecutable* runme = vector.at(0);
+    runme->run();
+    for (IExecutable* executable : vector) {
+        executable->run();
+    }
+    //====
+
     auto p1 = new Person();
     auto p2 = new Person("Bob", 19);
     
